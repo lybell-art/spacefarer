992eb2b2eec297098ae8556a3ce945c8bb262f89
lybell <32293736+lybell-art@users.noreply.github.com>
2021-05-09 16:57:29 +0900

    Update spacefarer.js

--- spacefarer.js
+++ spacefarer.js
@@ -222,7 +222,7 @@ function setup()
 	frameRate(60);
 	createCanvas(windowWidth,windowHeight,WEBGL);
 //	myCam=new lybellP5Camera(0, 0, 0, 0,0,1000);
-	myCam=new lybellP5Camera(0,-500,-50,0,0,0)
+	myCam=new lybellP5Camera(0,0,-500,0,0,0)
 	myCam.initialize();
 	bs=new blobSystem();
 	debugMode();
